16e3def8032b2939182974df08ffdb99d8244943
Alexander <efremov.alex@yahoo.bg>
2024-08-05 22:37:30 +0300

    05.08.2024

--- fruitipediaApp/fruitipediaApp/fruits/models.py
+++ fruitipediaApp/fruitipediaApp/fruits/models.py
@@ -1,3 +1,24 @@
+from django.core.validators import MinLengthValidator, RegexValidator
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(
+        unique=True,
+    )
+
+
+class Fruit(models.Model):
+    name = models.CharField(
+        max_length=30,
+        validators=[
+            MinLengthValidator(2),
+            RegexValidator(regex='^[a-zA-Z]+$', message="Fruit name should contain only letters!")
+        ]
+    )
+    Image_url = models.URLField()
+    description = models.TextField()
+    nutrition = models.TextField(
+        blank=True,
+        null=True
+    )

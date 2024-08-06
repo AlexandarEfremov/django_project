6c70dcf2e00fdcbc417a5c47365f9f3cd84096d9
Alexander <efremov.alex@yahoo.bg>
2024-08-06 17:55:04 +0300

    06.08.2024

--- fruitipediaApp/fruitipediaApp/fruits/urls.py
+++ fruitipediaApp/fruitipediaApp/fruits/urls.py
@@ -1,8 +1,15 @@
-from django.urls import path
+from django.urls import path, include
 
 from fruitipediaApp.fruits import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('dashboard/', views.dashboard, name='dashboard')
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('create-fruit/', views.create_view, name='create_fruit'),
+    path('create-category/', views.create_category, name='create_category'),
+    path('<int:pk>/', include([
+        path('edit-fruit/', views.edit_view, name='edit_fruit'),
+        path('delete-fruit/', views.delete_view, name='delete_fruit'),
+        path('details-fruits/', views.details_view, name='details_fruit'),
+    ]))
 ]
--- fruitipediaApp/fruitipediaApp/fruits/views.py
+++ fruitipediaApp/fruitipediaApp/fruits/views.py
@@ -13,15 +13,15 @@ def create_view(request):
     return render(request, 'fruits/create-fruit.html')
 
 
-def edit_view(request):
+def edit_view(request, pk):
     return render(request, 'fruits/edit-fruit.html')
 
 
-def details_view(request):
+def details_view(request, pk):
     return render(request, 'fruits/details-fruit.html')
 
 
-def delete_view(request):
+def delete_view(request, pk):
     return render(request, 'fruits/delete-fruit.html')
 
 
